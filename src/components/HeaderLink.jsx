4d83aa2eb2cdcd8c0a50cfdecfd3304d9f7a53c7
David-Viuche <jviucheroja@uniminuto.edu.co>
2023-07-11 12:52:55 -0500

    fix aria label error

--- src/components/HeaderLink.jsx
+++ src/components/HeaderLink.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export const HeaderLink = ({ children, href, target, ariaLabel, onClick }) => {
   return (
     <li onClick={onClick} className='w-4/5 rounded opacity-1  bg-slate-200 hover:bg-slate-400 sm:hover:bg-white sm:bg-white sm:hover:border-b-2 sm:w-auto flex justify-center'>
-      <Link href={href} target={target} ariaLabel={ariaLabel} className='w-full h-full flex justify-center p-2'>
+      <Link href={href} target={target} aria-label={ariaLabel} className='w-full h-full flex justify-center p-2'>
         {children}
       </Link>
     </li>
